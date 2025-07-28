020fb186134f31ab22f671511a83bf846b45dc3b
gouthamraj020102 <gouthamraj020102@gmail.com>
2025-07-28 16:54:50 +0530

    Use Case 2

--- src/main/java/com/conceptandcoding/learningspringboot/configuration/AsyncConfig.java
+++ src/main/java/com/conceptandcoding/learningspringboot/configuration/AsyncConfig.java
@@ -1,8 +1,26 @@
 package com.conceptandcoding.learningspringboot.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 public class AsyncConfig {
-    
+
+    @Bean(name = "myThreadPoolExecutor")
+    public Executor taskPoolExecutor() {
+        int minPoolSize = 2;
+        int maxPoolSize = 4;
+        int queueSize = 3;
+
+        ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
+        poolTaskExecutor.setCorePoolSize(minPoolSize);
+        poolTaskExecutor.setMaxPoolSize(maxPoolSize);
+        poolTaskExecutor.setQueueCapacity(queueSize);
+        poolTaskExecutor.setThreadNamePrefix("MyThread-");
+        poolTaskExecutor.initialize();
+        return poolTaskExecutor;
+    }
 }
