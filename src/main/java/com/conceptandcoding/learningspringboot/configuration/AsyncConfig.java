8d984c1a419d84c86e5ea0d18f746cbfd6ac70ef
gouthamraj020102 <gouthamraj020102@gmail.com>
2025-07-29 21:24:42 +0530

    Implementing Custom AsyncUncaughtExceptionHandler

--- src/main/java/com/conceptandcoding/learningspringboot/configuration/AsyncConfig.java
+++ src/main/java/com/conceptandcoding/learningspringboot/configuration/AsyncConfig.java
@@ -1,8 +1,31 @@
 package com.conceptandcoding.learningspringboot.configuration;
 
+import java.lang.reflect.Method;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.stereotype.Component;
 
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
+    @Autowired
+    private AsyncUncaughtExceptionHandler asyncUncaughtExceptionHandler;
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return this.asyncUncaughtExceptionHandler;
+    }
+}
+
+@Component
+class DefaultAsyncUncaughtExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+    @Override
+    public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+        System.out.println("in default Uncaught Exception method");
+        // logging can be done here.
+    }
 }
